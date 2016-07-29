97f53062150d19a3e93ab6e07f905e110b2c5808
masukomi <masukomi@masukomi.org>
2016-07-28 23:48:51 -0400

    Added ChangelogConfig#write_to(path : String)

--- src/changelog_config.cr
+++ src/changelog_config.cr
@@ -5,6 +5,11 @@ class ChangelogConfig
 		@ticket_url_prefix = nil as String?
 		@git_add = true as Bool
 	end
+
+	def write_to(path : String)
+		File.write(path, self.to_json)
+	end
+
 	# the following lets us ingest and expel json
 	JSON.mapping({
 		ticket_url_prefix:  {type: String, nilable: true},
