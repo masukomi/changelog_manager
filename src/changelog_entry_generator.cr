f1537ad6c12c0172503c923d4959dbc9a3923cfc
Kay Rhodes <krhodes@rbmtechnologies.com>
2016-08-24 22:14:17 -0400

    ChangelogEntryGenerator is now reusable by other code

--- src/changelog_entry_generator.cr
+++ src/changelog_entry_generator.cr
@@ -2,7 +2,32 @@ require "./cli_tool"
 class ChangelogEntryGenerator
 	include CliTool
 
+	def get_changelog_entry_from_input(cd : ChangelogDatabase, 
+									   config : ChangelogConfig ) : ChangelogEntry
+		change_type = get_change_type()
 
+		ticket      = Readline.readline("Ticket ID? (optional): ")
+		url         = nil as String?
+		if ticket.to_s != "" && config.ticket_url_prefix
+			url = config.ticket_url_prefix.to_s + ticket.to_s
+		else
+			url         = Readline.readline("Ticket URL? (optional): ")
+		end
+		description = ask_for_non_optional_input("Describe your change: ")
+		description = description.gsub(/\\\\n/, "\\n")
+		# enable descriptions with encoded newlines
+
+		raw_tags = Readline.readline("Tags (Optional, comma separated): ")
+		tags = raw_tags.to_s.split(/,\s*/).select{|x| !x.nil? && x != ""} 
+		# I don't trust them to follow instructions
+
+		changelog_entry = ChangelogEntry.new(change_type, 
+										description,
+										ticket.to_s == "" ? nil : ticket.to_s,
+										url.to_s == "" ? nil : url.to_s,
+										tags)
+		return changelog_entry
+	end
 	def run()
 
 		get_stdin_from_tty()
@@ -17,26 +42,7 @@ class ChangelogEntryGenerator
 		config = cd.get_config()
 
 
-		change_type = get_change_type()
-
-		ticket      = Readline.readline("Ticket ID? (optional): ")
-		url         = nil as String?
-		if ticket.to_s != "" && config.ticket_url_prefix
-			url = config.ticket_url_prefix.to_s + ticket.to_s
-		else
-			url         = Readline.readline("Ticket URL? (optional): ")
-		end
-		description = ask_for_non_optional_input("Describe your change: ")
-		# TODO support tags
-		raw_tags = Readline.readline("Tags (Optional, comma separated): ")
-		tags = raw_tags.to_s.split(/,\s*/).select{|x| !x.nil? && x != ""} 
-		# i don't trust them to follow instructions
-
-		changelog_entry = ChangelogEntry.new(change_type, 
-										description,
-										ticket.to_s == "" ? nil : ticket.to_s,
-										url.to_s == "" ? nil : url.to_s,
-										tags)
+		changelog_entry = get_changelog_entry_from_input(cd, config)
 		# puts changelog_entry.to_json
 		new_entry_location = changelog_entry.export(cd)
 		success = false
